9bdd3c5b37253166d5ea1f9ec6cfefaea4409f6e
Nazrul <heynazrul@gmail.com>
2023-06-12 01:01:40 +0600

    added private route

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,32 @@
+import { Navigate, useLocation } from "react-router";
+import useAuth from "../hooks/useAuth";
+import loaderAnimation from '../assets/lottie/loader.json'
+import Lottie from 'lottie-react';
+
+const PrivateRoute = ({children}) => {
+    const {user, loading} = useAuth()
+    
+     const location = useLocation();
+
+     if (loading) {
+       return (
+         <div className="h-[calc(100vh-100px)] flex items-center">
+           <div className="w-52 mx-auto">
+             <Lottie animationData={loaderAnimation}></Lottie>
+           </div>
+         </div>
+       );
+     }
+
+     if (user) {
+       return children;
+     }
+     return (
+       <Navigate
+         to="/login"
+         state={{ from: location }}
+         replace></Navigate>
+     );
+};
+
+export default PrivateRoute;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -14,6 +14,7 @@ import MyClasses from '../pages/Dashboard/Instructor/MyClasses';
 import ManageClasses from '../pages/Dashboard/Admin/ManageClasses';
 import ManageUsers from '../pages/Dashboard/Admin/ManageUsers';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
+import PrivateRoute from './PrivateRoute';
 
 export const router = createBrowserRouter([
   {
@@ -39,7 +40,11 @@ export const router = createBrowserRouter([
       },
       {
         path: 'classes',
-        element: <Classes></Classes>,
+        element: (
+          <PrivateRoute>
+            <Classes></Classes>
+          </PrivateRoute>
+        ),
       },
     ],
   },
